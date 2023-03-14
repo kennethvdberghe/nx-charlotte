2b974ea1edb15b8a4235fcc0d0e88d0e0010cc88
Kenneth Van den Berghe <kenneth.vandenberghe@optis.be>
2023-03-14 10:01:12 +0100

    get a joke

--- apps/my-app/src/app/app.tsx
+++ apps/my-app/src/app/app.tsx
@@ -1,10 +1,16 @@
-import { getFakeJSON } from '@nx-charlotte/api';
+import { getFakeJSON, getJoke } from '@nx-charlotte/api';
 import { useQuery } from '@tanstack/react-query';
 
 export function App() {
   const query = useQuery({ queryKey: ['todos'], queryFn: getFakeJSON });
+  const jokeQuery = useQuery({ queryKey: ['joke'], queryFn: getJoke });
 
-  return <div>{JSON.stringify(query.data)}</div>;
+  return (
+    <div>
+      <p>{JSON.stringify(query.data)}</p>
+      <p>{JSON.stringify(jokeQuery.data?.value)}</p>
+    </div>
+  );
 }
 
 export default App;
